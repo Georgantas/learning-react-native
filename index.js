4382d830bada8b9b2ca0b70e5f037ee44714670e
Georgantas <john.georan@icloud.com>
2017-10-24 12:28:36 -0400

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('albums', () => App);
+
+// note: index.ios.js - config/entry file for ios
+// note: index.android.js - config/entry file for android
