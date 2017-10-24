fbfc51be9eefc9d2393c7398df469ec5f4505b9b
Georgantas <john.georan@icloud.com>
2017-10-24 15:30:53 -0400

    progress.

--- App.js
+++ App.js
@@ -1,1 +1,20 @@
 
+// import a library to help create a component
+
+import React from 'react';
+import { View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+// create a component
+const App = () => {
+  return (
+    <View>
+      <Header headerText={'Albums'} />
+      <AlbumList />
+    </View>
+  );
+};
+
+// render the component to the device
+export default App;
